62d99e7f59dd8282481c29436f5f9722d4c6f795
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-15 21:26:41 -0400

    adding query strings

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
 <div class="row">
   <div class="col">
     <nav class="nav">
-      <a class="nav-link" href="https://brooklynrail.org/" title="Current Issue">Current Issue</a>
-      <a class="nav-link" href="https://brooklynrail.org/about/" title="About the Rail">About the Rail</a>
+      <a class="nav-link" href="https://brooklynrail.org/?=support" title="Current Issue">Current Issue</a>
+      <a class="nav-link" href="https://brooklynrail.org/about/?=support" title="About the Rail">About the Rail</a>
     </nav>
   </div>
 </div>
