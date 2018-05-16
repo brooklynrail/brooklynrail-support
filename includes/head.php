a98f7e5f30084a4d37e924cb8bea81b8d54bafd1
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-15 21:01:14 -0400

    editing SEO meta tags

--- includes/head.php
+++ includes/head.php
@@ -4,28 +4,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Begin Jekyll SEO tag v2.4.0 -->
-<title>Support the Brooklyn Rail | Write an awesome description for your new site here. You can edit this line in _config.yml. It will appear in your document head meta (for Google search results) and in your feed.xml site description.</title>
-<meta name="generator" content="Jekyll v3.7.2" />
-<meta property="og:title" content="Support the Brooklyn Rail" />
-<meta property="og:locale" content="en_US" />
-<meta name="description" content="Write an awesome description for your new site here. You can edit this line in _config.yml. It will appear in your document head meta (for Google search results) and in your feed.xml site description." />
-<meta property="og:description" content="Write an awesome description for your new site here. You can edit this line in _config.yml. It will appear in your document head meta (for Google search results) and in your feed.xml site description." />
-<link rel="canonical" href="/" />
-<meta property="og:url" content="/" />
-<meta property="og:site_name" content="Support the Brooklyn Rail" />
-<script type="application/ld+json">
-{"headline":"Support the Brooklyn Rail","description":"Write an awesome description for your new site here. You can edit this line in _config.yml. It will appear in your document head meta (for Google search results) and in your feed.xml site description.","name":"Support the Brooklyn Rail","@type":"WebSite","url":"/","@context":"http://schema.org"}</script>
-<!-- End Jekyll SEO tag -->
+  <title>Support the Brooklyn Rail</title>
+  <meta property="og:title" content="Support the Brooklyn Rail" />
+  <meta property="og:locale" content="en_US" />
+  <meta name="description" content="Keep the Rail alive and free! Support independent publishing." />
+  <meta property="og:description" content="Keep the Rail alive and free! Support independent publishing." />
+  <link rel="canonical" href="/" />
+  <meta property="og:url" content="/" />
+  <meta property="og:image" content="assets/img/quote.png" />
+  <meta property="og:site_name" content="The Brooklyn Rail" />
+  <script type="application/ld+json">
+  {"headline":"Support the Brooklyn Rail","description":"Keep the Rail alive and free! Support independent publishing.","name":"Support the Brooklyn Rail","@type":"WebSite","url":"/","@context":"http://schema.org"}</script>
 
-<!-- Braintree -->
-    <!-- <link rel=stylesheet type=text/css href="static/css/app.css"> -->
-    <!-- <link rel=stylesheet type=text/css href="static/css/overrides.css"> -->
-
-
+  <!-- CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/main.css">
-  <link rel="stylesheet" href="assets/popup.css">
 
   <!-- Roboto Mono — Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i,700,700i" rel="stylesheet">
