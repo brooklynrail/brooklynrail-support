84197b551fb687a4da88fa25f10c20b0e0826f50
WebDev BkRail <webdev@brooklynrail.org>
2018-05-23 17:40:18 -0700

    make directory agnostic

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 
-if(file_exists(__DIR__ . "/../../../.env-support")) {
-    $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", '.env-support');
+$dirname = basename(dirname(__DIR__, 1));
+
+if(file_exists(__DIR__ . "/../../../.env-$dirname")) {
+    $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", ".env-$dirname");
     $dotenv->load();
 }
