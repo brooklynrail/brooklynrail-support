ca5a4058ad7a7a6cf9ed236b7e1b9473d129ced9
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-29 22:03:54 -0400

    add in receipt

--- includes/response.php
+++ includes/response.php
@@ -1,13 +1,34 @@
 <section id="donate" class="response">
   <div class="container">
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col-sm-12 col-md-8">
         <!-- Start Braintree -->
         <h1><?php echo($header)?></h1>
         <p class="message"><?php echo($message)?></p>
-        <a class="btn btn-lg btn-primary" href="https://brooklynrail.org/">Current Issue</a>
+
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <div class="receipt">
+              <p><strong>The Brooklyn Rail</strong></p>
+              <hr/>
+              <p><?php echo($transaction->createdAt->format('F j, Y'))?> at <?php echo($transaction->createdAt->format('g:i a'))?></p>
+              <p>Transaction id: <?php echo($transaction->id)?></p>
+              <p>Name: [ <?php echo($transaction->customer->firstName)?> <?php echo($transaction->customer->lastName)?> ]</p>
+              <p>Amount: $<?php echo($transaction->amount)?></p>
+              <hr/>
+              <p>Thank you</p>
+            </div>
+          </div>
+        </div>
         <!-- end Braintree -->
       </div>
     </div>
+
+    <div class="row">
+      <div class="col-sm-12 col-md-8">
+        <a class="btn btn-lg btn-primary" href="https://brooklynrail.org/">Go to current issue</a>
+      </div>
+    </div>
+    
   </div>
 </section>
