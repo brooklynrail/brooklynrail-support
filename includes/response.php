16f3c415d7b4046231b5336ca64eec7ca51755e2
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-30 22:15:07 -0400

    adding in PayPal

--- includes/response.php
+++ includes/response.php
@@ -7,17 +7,31 @@
         <p class="message"><?php echo($message)?></p>
 
         <div class="row">
-          <div class="col-sm-12 col-md-6">
+          <div class="col-sm-12 col-md-7">
             <div class="receipt">
               <p><strong>The Brooklyn Rail</strong></p>
               <hr/>
               <p><?php echo($transaction->createdAt->format('F j, Y'))?> at <?php echo($transaction->createdAt->format('g:i a'))?></p>
               <p>Transaction id: <?php echo($transaction->id)?></p>
-              <p class="hide">Name: [ <?php echo($transaction->customer->firstName)?> <?php echo($transaction->customer->lastName)?> ]</p>
+
+              <?php if (strpos(json_encode($transaction), 'customer')) { ?>
+                <p>Name: <?php echo($transaction->customer['firstName']); ?> <?php echo($transaction->customer['lastName']); ?></p>
+              <?php } ?>
+
+              <?php if (strpos(json_encode($transaction), 'paypal')) { ?>
+                <p>PayPal: <?php echo($transaction->paypal['payerEmail'])?></p>
+              <?php } ?>
+
               <p>Amount: $<?php echo($transaction->amount)?></p>
+
               <hr/>
               <p>Thank you</p>
             </div>
+
+            <?php //print_r('======================'); ?>
+            <?php //print_r($transaction); ?>
+            <?php //print_r('======================'); ?>
+
           </div>
         </div>
         <!-- end Braintree -->
