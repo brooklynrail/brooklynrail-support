46a4a3afdd665495678511d3860835e9353d0a43
Jeremy Zilar <jeremyzilar@gmail.com>
2018-06-05 23:34:36 -0400

    removing EB Garamond Typeface (for now)

--- includes/head.php
+++ includes/head.php
@@ -20,7 +20,8 @@
   <link rel="stylesheet" href="assets/main.css?=20180530">
 
   <!-- Libre Franklin + EB Garamond — Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i|Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i|Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+  <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <link type="application/atom+xml" rel="alternate" href="/feed.xml" title="Support the Brooklyn Rail" />
 
