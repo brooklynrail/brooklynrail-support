bc2b95b536ab848e0de8bcb09877292aed788e91
Jeremy Zilar <jeremyzilar@gmail.com>
2018-06-07 18:13:46 -0400

    update donate text

--- includes/donate.php
+++ includes/donate.php
@@ -26,7 +26,7 @@
               <button class="button btn btn-primary btn-lg" type="submit"><span>Pay</span></button>
             </form>
             <div class="meta">
-              <p>Questions? Email us at <a href="mailto:store@brooklynrail.org?subject=Donation%20Question">store@brooklynrail.org</a></p>
+              <p>The <em>Brooklyn Rail</em> is a nonprofit 501(c)(3) organization. Your donation is fully deductible. Donations are non-refundable. Questions? Email us at <a href="mailto:store@brooklynrail.org?subject=Donation%20Question">store@brooklynrail.org</a></p>
               <p><a href="https://brooklynrail.org/contact" title="Contact the Rail">Contact Us</a> | <a title="Terms of Service" href="https://brooklynrail.org/terms-of-service">Terms of Service</a> | <a href="https://store.brooklynrail.org/store_web_pages/index/14" title="Privacy Policy">Privacy Policy</a></p>
             </div>
 
