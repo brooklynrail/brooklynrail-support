95358f7aab904d8337830b347f713dcf3244beb0
Jeremy Zilar <jeremyzilar@gmail.com>
2018-06-27 13:13:16 -0400

    email validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,5 +34,59 @@
     });
 
 
+    // send-friend
+
+    // Validate Email(s)
+    function validatemail(mail) {
+      var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      if (mail.match(mailformat)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+
+    $( '#email' ).keyup(function() {
+      var email = $(this).val();
+      var valid = validatemail(email.trim());
+      if (valid == true) {
+        $(this).addClass('is-valid').removeClass('is-invalid').focus();
+        $('#submit-button').removeAttr('disabled');
+      } else {
+        $(this).addClass('is-invalid').removeClass('is-valid').focus();
+        $('#submit-button').prop('disabled', true);
+      }
+    });
+
+    $( '#emails' ).keyup(function() {
+      var raw = $(this).val();
+      var emails = raw.split(",");
+      var valid = true;
+      emails.forEach(function (email) {
+        var v = validatemail(email.trim());
+        if (v == false) {
+          valid = false;
+        }
+      });
+      if (valid == true) {
+        $(this).addClass('is-valid').removeClass('is-invalid').focus();
+        $('#submit-button').removeAttr('disabled');
+      } else {
+        $(this).addClass('is-invalid').removeClass('is-valid').focus();
+        $('#submit-button').prop('disabled', true);
+      }
+      $('#submit-button').attr('href', build_email(raw))
+    });
+
+    function build_email(emails) {
+      var subject = "KEEP THE RAIL ALIVE AND FREE";
+      var body = "hello!";
+      var string = 'mailto:'+emails+'?subject='+subject+'?body='+body+'';
+      var href = encodeURI(string);
+      return href;
+    }
+
+
   });
 })(jQuery);
