aafc10c1c18e5a0bd11a5b10f97e6f39cc021ddb
Jeremy Zilar <jeremyzilar@gmail.com>
2018-06-27 13:14:10 -0400

    send to friend HTML

---
+++ includes/send-friend.php
@@ -0,0 +1,18 @@
+<div class="row" id="send-friend">
+  <div class="col-sm-12 col-md-7">
+    <div class="box">
+      <h5><img src="/assets/img/email-icon.png" alt=""> Send to a friend</h5>
+      <p>Encourage others to <strong>KEEP THE <em>RAIL</em> ALIVE AND FREE</strong> by sending them an email with our donate link.</p>
+
+      <form class="" action="" method="post">
+        <div class="input-group mb-3">
+          <input id="emails" type="text" class="form-control" placeholder="Recipient's Email" aria-label="Recipient's email" >
+          <div class="input-group-append">
+            <a href="#" class="btn btn-primary" id="submit-button" type="button" disabled>Send</a>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  </div>
+</div>
