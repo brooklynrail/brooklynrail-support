0efd07d36ea4609f2f2382b3d890e50655072659
Jeremy Zilar <jeremyzilar@gmail.com>
2018-08-10 15:38:31 -0400

    adding env config for localhost

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -3,7 +3,15 @@ session_start();
 
 $dirname = basename(dirname(__DIR__, 1));
 
-if(file_exists(__DIR__ . "/../../../.env-$dirname")) {
+
+if ($_SERVER['HTTP_HOST'] === 'local.work:3000'){
+  if(file_exists(__DIR__ . "/../.env")) {
+    $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env");
+    $dotenv->load();
+  }
+} else {
+  if(file_exists(__DIR__ . "/../../../.env-$dirname")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", ".env-$dirname");
     $dotenv->load();
+  }
 }
