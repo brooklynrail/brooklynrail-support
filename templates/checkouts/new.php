a9d19622ff2a859de5823c82df7d178ad58d61f3
dmgig <dave.m.giglio@gmail.com>
2019-02-05 12:57:29 -0500

    fix merge issue

--- templates/checkouts/new.php
+++ templates/checkouts/new.php
@@ -1,4 +1,4 @@
-<?php $clientToken = $this->data["client_token"]; 
+<?php $clientToken = $this->data["client_token"];
 $supportPath = $this->data["support_path"];
 
 ?>
@@ -7,31 +7,7 @@ $supportPath = $this->data["support_path"];
 <body>
 <?php require_once("includes/header.php"); ?>
 
-<<<<<<< HEAD
-<section id="donate">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12 col-md-8">
-
-        <!-- Start Braintree -->
-        <div class="wrapper">
-          <div class="checkout">
-            <form method="post" id="payment-form" action="/">
-              <section>
-                <label for="amount">
-                  <span class="input-label">Give $10, $25, $50 +</span>
-                  <div class="input-wrapper amount-wrapper">
-                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="25">
-                  </div>
-                </label>
-
-                <div class="bt-drop-in-wrapper">
-                  <div id="bt-dropin"></div>
-                </div>
-              </section>
-=======
 <?php require_once("includes/content.php"); ?>
->>>>>>> master
 
 <?php require_once("includes/donate.php"); ?>
 
