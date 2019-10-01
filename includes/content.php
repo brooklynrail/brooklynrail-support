8a70f6c1bcaa922beabafd97e69e2f52871ca3f8
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-01 09:19:40 -0400

    adding a space

--- includes/content.php
+++ includes/content.php
@@ -5,6 +5,7 @@
 
         <div class="entry-content">
           <p>In nineteen years, we have never wavered in our commitment to free distribution. Individual donors are integral to our journalistic independence, and allow us to put critical and imaginative writing in the hands of the public.</p>
+          
           <p>Please consider donating to help keep the <em>Rail</em> free for all.</p>
         </div>
 
