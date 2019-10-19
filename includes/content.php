cbe1f7103b7bc7207d4758077b32f0f6f99e6956
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-19 11:24:41 -0400

    copy changes

--- includes/content.php
+++ includes/content.php
@@ -5,8 +5,8 @@
 
         <div class="entry-content">
           <p>In nineteen years, we have never wavered in our commitment to free distribution. Individual donors are integral to our journalistic independence, and allow us to put critical and imaginative writing in the hands of the public.</p>
-          
-          <p>Please consider donating to help keep the <em>Rail</em> free for all.</p>
+
+          <p><strong>Please donate to help keep the <em>Rail</em> independent and free.</strong></p>
         </div>
 
       </div>
