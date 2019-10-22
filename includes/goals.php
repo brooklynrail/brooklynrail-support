fba842ad4ad843ede4da876ff762d7f2e525fadf
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-22 16:15:56 -0400

    pushing new goal logic

--- includes/goals.php
+++ includes/goals.php
@@ -1,8 +1,12 @@
 <?php
-  $current_amt = '10150';
+  $current_amt = '1500';
   $total_amt = '50000';
-  $backers = '22';
+  $backers = '3';
   $percentChange = number_format(($current_amt / $total_amt) * 100, 0);
+  $future = date_create("2019-12-31");
+  $now = date_create(date("Y/m/d"));
+  $diff = date_diff($future,$now);
+  $days_left = $diff->format("%a");
 ?>
 
 <section class="goals">
@@ -13,5 +17,6 @@
 
   <p class="current_amt">$<?php echo number_format($current_amt); ?> <span>donated out of $<?php echo number_format($total_amt); ?></span></p>
   <p><?php echo $backers; ?> <span>donors</span></p>
+  <p><?php echo $days_left; ?> <span>days remaining</span></p>
 
 </section>
