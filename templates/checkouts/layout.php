ee76d969b33cdcfa7bb83ce2e55069cf1191829b
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-26 15:22:30 -0400

    adjusting the top styles

--- templates/checkouts/layout.php
+++ templates/checkouts/layout.php
@@ -16,10 +16,9 @@ $supportPath = $this->data["support_path"];
           <div class="grid-row">
             <div class="grid-col-12">
               <div class="usa-prose">
-                <p>For over 19 years, individual donations from artists, writers, and friends, and patrons have been integral to our journalistic independence.</p>
+                <p>We aim to raise $<span>50,000</span> by <span>December 31st</span> to <strong>keep the <em>Rail</em> independent and free.</strong></p>
 
-                <p>We aim to raise $<span>50,000</span> by <span>December 31st</span> to expand our reach and help <strong>keep the <em>Rail</em> independent and free.</strong></p>
-
+                <p>For over 19 years, individual donations from artists, writers, friends, and patrons have been integral to our independence.</p>
               </div>
             </div>
           </div>
