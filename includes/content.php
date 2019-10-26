8f6ccad857c082c77d4e896bd8a7c54d426a7e29
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-26 17:16:37 -0400

    new progress bar

--- includes/content.php
+++ includes/content.php
@@ -1,10 +1,12 @@
 <div id="content" class="usa-prose">
 
 
-  <p>This month marks the nineteenth anniversary of The Brookly Rail’s creation. This past year has been a monumental one, with curatorial projects at both the Venice Biennale and the Colby Museum, two special issues of the River Rail, three new releases from our literary press Rail Editions, and the continued publication of our ten annual issues, now read each month by 200,000 individuals around the world.</p>
+  <p>This month marks the nineteenth anniversary of The Brooklyn Rail’s creation. This past year has been a monumental one, with curatorial projects at both the <a href="https://venice.brooklynrail.org">Venice Biennale</a> and the Colby Museum, two special issues of the River Rail, <a href="https://shop.brooklynrail.org">three new releases from our literary press Rail Editions</a>, and the continued publication of our <a href="https://brooklynrail.org/subscribe">ten annual issues</a>, now read each month by 200,000 individuals around the world.</p>
 
   <p>This year has also come with challenges to free speech, artistic freedom, and our democratic system. Now more than ever, the principles of journalistic independence and egalitarianism that grounded the founding of the Rail are under threat. It is with this in mind that I respectfully ask for your support.</p>
 
-  <p>We aim to raise $50,000 by December 31st from members of our community. As a nonprofit, the support of artists, writers, friends, and patrons allows us to expand our reach while compensating the hundreds of contributors whose words appear in every issue.</p>
+  <p><span>We aim to raise $50,000 by December 31st from members of our community.</span> As a nonprofit, the support of artists, writers, friends, and patrons allows us to expand our reach while compensating the hundreds of contributors whose words appear in every issue.</p>
+
+  <p><strong>Please help to keep the Rail independent and free.</strong></p>
 
 </div>
