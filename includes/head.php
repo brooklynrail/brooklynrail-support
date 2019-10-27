48da99409174c56eccda02c250888a105d2255e4
Jeremy Zilar <jeremyzilar@gmail.com>
2019-10-27 18:44:46 -0400

    editing page meta info

--- includes/head.php
+++ includes/head.php
@@ -4,26 +4,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Support the Brooklyn Rail</title>
+  <title>Donate to the Brooklyn Rail</title>
   <meta property="og:title" content="Support the Brooklyn Rail" />
   <meta property="og:locale" content="en_US" />
-  <meta name="description" content="Keep the Rail alive and free! Support independent publishing." />
-  <meta property="og:description" content="Keep the Rail alive and free! Support independent publishing." />
+  <meta name="description" content="Help us reach $50,000 by the December 31, 2019 to keep the Rail independent and free!" />
+  <meta property="og:description" content="Help us reach $50,000 by the December 31, 2019 to keep the Rail independent and free!" />
   <meta property="og:url" content="https://brooklynrail.org/donate/" />
   <meta property="og:type" content="website" />
-  <meta property="og:image" content="https://brooklynrail.org/donate/assets/img/keep-the-rail-alive.png" />
+  <meta property="og:image" content="assets/img/brooklynrail-card.png" />
   <meta property="og:site_name" content="The Brooklyn Rail" />
   <script type="application/ld+json">
-  {"headline":"Support the Brooklyn Rail","description":"Keep the Rail alive and free! Support independent publishing.","name":"Support the Brooklyn Rail","@type":"WebSite","url":"/","@context":"http://schema.org"}</script>
-
+  {"headline":"Donate to the Brooklyn Rail","description":"Help us reach $50,000 by the December 31, 2019 to keep the Rail independent and free!","name":"Donate to the Brooklyn Rail","@type":"WebSite","url":"/","@context":"http://schema.org"}</script>
 
   <?php $cachetime = filemtime("assets/dist/styles.css"); ?>
   <!-- CSS -->
   <link rel="stylesheet" href="assets/dist/styles.css?<?= $cachetime ?>">
-
-  <!-- Libre Franklin + EB Garamond — Google Fonts -->
-  <!-- <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i|Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
-  <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- fontawesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
