ad0e76ca8f5d7494983c4d2ceb20fbf04bacf354
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-01 17:49:51 -0400

    removing tests, crossing fingers

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -2,10 +2,7 @@
 session_start();
 
 $dirname = basename(dirname(__DIR__));
-print_r($dirname);
-echo "<br/>";
-print_r(__DIR__);
-echo "<br/>";
+
 if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
   if(file_exists(__DIR__ . "/../.env-support-beta")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-support-beta");
@@ -13,9 +10,7 @@ if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
   }
 } else {
   if(file_exists(__DIR__ . "/../.env-donate")) {
-    echo "yes";
-    die();
-    // $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-donate");
-    // $dotenv->load();
+    $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-donate");
+    $dotenv->load();
   }
 }
