c48371916f7edc31a25b77d5a40ed0890a8f46b9
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-01 17:43:59 -0400

    adding line breaks

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -3,8 +3,9 @@ session_start();
 
 $dirname = basename(dirname(__DIR__));
 print_r($dirname);
+echo "<br/>"
 print_r(__DIR__);
-
+echo "<br/>"
 if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
   if(file_exists(__DIR__ . "/../.env-support-beta")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-support-beta");
@@ -13,8 +14,10 @@ if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
 } else {
   if(file_exists(".env-donate")) {
     print_r('yes');
+    echo "<br/>"
     print_r(__DIR__);
-    print_r('|  | ');
+    echo "<br/>"
+    echo "<br/>"
     print_r(__DIR__ . "/../../../");
     // $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", ".env-donate");
     // $dotenv->load();
