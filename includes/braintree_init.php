c9e9c81fbfe288d10b58c1c2204bff319519b45e
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-01 17:33:42 -0400

    removing pipe

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -12,8 +12,8 @@ if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
 } else {
   if(file_exists(__DIR__ . "../.env-donate")) {
     print_r('yes');
-    print_r(__DIR__); |
-    print_r('yes');
+    print_r(__DIR__);
+    print_r('|  | ');
     print_r(__DIR__ . "/../../../");
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", ".env-donate");
     $dotenv->load();
