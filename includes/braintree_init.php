e5f35909d012ba7b3fc76634e9f0634fb7100b15
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-01 17:46:18 -0400

    adding another test

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -3,23 +3,19 @@ session_start();
 
 $dirname = basename(dirname(__DIR__));
 print_r($dirname);
-echo "<br/>"
+echo "<br/>";
 print_r(__DIR__);
-echo "<br/>"
+echo "<br/>";
 if ($_SERVER['HTTP_HOST'] === 'localhost:3000'){
   if(file_exists(__DIR__ . "/../.env-support-beta")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-support-beta");
     $dotenv->load();
   }
 } else {
-  if(file_exists(".env-donate")) {
-    print_r('yes');
-    echo "<br/>"
-    print_r(__DIR__);
-    echo "<br/>"
-    echo "<br/>"
-    print_r(__DIR__ . "/../../../");
-    // $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../../", ".env-donate");
+  if(file_exists("../.env-donate")) {
+    echo "yes";
+    die();
+    // $dotenv = new Dotenv\Dotenv(__DIR__ . "/../", ".env-donate");
     // $dotenv->load();
   }
 }
