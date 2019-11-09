7ef2ca3f9663022b4f813854c7f55ad583ebacd0
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-09 17:52:49 -0500

    fixing links in footer

--- includes/footer.php
+++ includes/footer.php
@@ -10,24 +10,24 @@
       <div class="grid-col-6 tablet-lg:grid-col-3 tablet-lg:grid-offset-1 desktop:grid-col-2 desktop:grid-offset-3">
         <h5>The RAIL</h5>
         <ul>
-          <li><a href="/about/?f">About the Rail</a></li>
-          <li><a href="/staff/?f">Staff</a></li>
-          <li><a href="/supporters/?f">Supporters</a></li>
-          <li><a href="/contributors/?f">Contributors</a></li>
+          <li><a href="https://brooklynrail.org/about/?f">About the Rail</a></li>
+          <li><a href="https://brooklynrail.org/staff/?f">Staff</a></li>
+          <li><a href="https://brooklynrail.org/supporters/?f">Supporters</a></li>
+          <li><a href="https://brooklynrail.org/contributors/?f">Contributors</a></li>
           <li><a href="https://shop.brooklynrail.org?f">Store</a></li>
-          <li><a href="/history/?f">History</a></li>
-          <li><a href="/archives/?f">Archives</a></li>
-          <li><a href="/contact/?f">Contact Us</a></li>
+          <li><a href="https://brooklynrail.org/history/?f">History</a></li>
+          <li><a href="https://brooklynrail.org/archives/?f">Archives</a></li>
+          <li><a href="https://brooklynrail.org/contact/?f">Contact Us</a></li>
         </ul>
       </div>
       <div class="grid-col-6 tablet-lg:grid-col-3 desktop:grid-col-2">
         <h5>Get Involved</h5>
         <ul>
           <li><a href="https://mailchi.mp/brooklynrail/join/?f">Sign up for our newsletter</a></li>
-          <li><a href="/subscribe/?f">Subscribe</a></li>
+          <li><a href="https://brooklynrail.org/subscribe/?f">Subscribe</a></li>
           <li><a href="https://donate.brooklynrail.org/?f">Donate</a></li>
-          <li><a href="/advertise/?f">Advertise</a></li>
-          <li><a href="/submissions/?f">Submissions</a></li>
+          <li><a href="https://brooklynrail.org/advertise/?f">Advertise</a></li>
+          <li><a href="https://brooklynrail.org/submissions/?f">Submissions</a></li>
         </ul>
       </div>
       <div class="grid-col-12 tablet-lg:grid-col-2">
@@ -36,7 +36,7 @@
           <li><a href="https://www.instagram.com/brooklynrail/" title="Follow @brooklynrail on Instagram"><i class="fab fa-instagram"></i> Instagram</a></li>
           <li><a href="https://twitter.com/thebrooklynrail" title="Follow @thebrooklynrail on Twitter"><i class="fab fa-twitter-square"></i> Twitter</a></li>
           <li><a href="https://www.facebook.com/thebrooklynrail" title="Like The Brooklyn Rail on Facebook"><i class="fab fa-facebook-f"></i> Facebook</a></li>
-          <li><a href="/rss" title="The Brooklyn Rail RSS feed"><i class="fas fa-rss"></i> RSS</a></li>
+          <li><a href="https://brooklynrail.org/rss" title="The Brooklyn Rail RSS feed"><i class="fas fa-rss"></i> RSS</a></li>
         </ul>
       </div>
     </div>
