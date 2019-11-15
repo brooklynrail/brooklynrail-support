a1bce4b9bc1214f24c90d9c88e71c8b6d38c2a6f
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-15 16:25:43 -0500

    updating to $18,222 / 55 backers

--- fundraiser.php
+++ fundraiser.php
@@ -3,9 +3,9 @@ header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
 $data = array(
-  "donated" => 16517,
+  "donated" => 18222,
   "goal" => 50000,
-  "backers" => 49,
+  "backers" => 55,
 );
 
 echo $_GET['callback'] . '('.json_encode($data).')';
