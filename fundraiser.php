e12b6165f49d59007db36886f359f956fc49b131
Jeremy Zilar <jeremyzilar@gmail.com>
2019-11-19 22:57:14 -0500

    updating the

--- fundraiser.php
+++ fundraiser.php
@@ -3,9 +3,9 @@ header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
 $data = array(
-  "donated" => 20207,
+  "donated" => 20387,
   "goal" => 50000,
-  "backers" => 70,
+  "backers" => 74,
 );
 
 echo $_GET['callback'] . '('.json_encode($data).')';
