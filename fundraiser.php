eca28f404fe888b28711e387a233d07775c82b12
Jeremy Zilar <jeremyzilar@gmail.com>
2019-12-11 20:37:18 -0500

    Updated to $30,398 from 145 backers

--- fundraiser.php
+++ fundraiser.php
@@ -2,10 +2,27 @@
 header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
+// Amount donated
+$donated = '30398';
+
+// Backers / Donors
+$backers = '145';
+
+// Goal
+$goal = '50000';
+
+$percentChange = number_format(($donated / $goal) * 100, 0);
+$future = date_create("2019-12-31");
+$now = date_create(date("Y/m/d"));
+$diff = date_diff($future,$now);
+$days_left = $diff->format("%a");
+
 $data = array(
-  "donated" => 29818,
-  "goal" => 50000,
-  "backers" => 141,
+  "donated" => $donated,
+  "goal" => $goal,
+  "backers" => $backers,
+  "percentChange" => $percentChange,
+  "days_left" => $days_left,
 );
 
 echo $_GET['callback'] . '('.json_encode($data).')';
