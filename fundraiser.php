12e32d458415a9170c28e13ab88bece13d77b338
Jeremy Zilar <jeremyzilar@gmail.com>
2019-12-29 20:59:52 -0500

    Updated to $74,525 from 232 lovely people

--- fundraiser.php
+++ fundraiser.php
@@ -3,10 +3,10 @@ header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
 // Amount donated
-$donated = '72856';
+$donated = '74525';
 
 // Backers / Donors
-$backers = '228';
+$backers = '232';
 
 // Goal
 $goal = '50000';
