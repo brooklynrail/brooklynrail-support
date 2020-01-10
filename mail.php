98344198c9894af569996ab95d1ac96f3fad15ed
Jeremy Zilar <jeremyzilar@gmail.com>
2020-01-09 23:51:02 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,8 +3,14 @@
 $secret="6LfHB84UAAAAACwdUwV66zn_R12MONau7XxDcY9E";
 $response=$_POST["captcha"];
 
-$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
+$url = "https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}";
+curl_setopt($ch=curl_init(), CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$verify = curl_exec($ch);
+curl_close($ch);
+
 $captcha_success=json_decode($verify);
+
 if ($captcha_success->success==false) {
   //This user was not verified by recaptcha.
 
