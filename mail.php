456219a1dcee4a5e8ad8d985c408aea7e3f55b01
Jeremy Zilar <jeremyzilar@gmail.com>
2020-01-09 23:43:29 -0500

    adding in json response

--- mail.php
+++ mail.php
@@ -1,15 +1,25 @@
 <?php
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
+
 // ReCaptcha set up
 $secret="6LfHB84UAAAAACwdUwV66zn_R12MONau7XxDcY9E";
 $response=$_POST["captcha"];
 
 $verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
 $captcha_success=json_decode($verify);
+
+
 if ($captcha_success->success==false) {
-  //This user was not verified by recaptcha.
+  $data = array(
+    "success" => false,
+  );
+  echo $_GET['callback'] . '('.json_encode($data).')';
 
 }
 else if ($captcha_success->success==true) {
-  //This user is verified by recaptcha
-
+  $data = array(
+    "success" => true,
+  );
+  echo $_GET['callback'] . '('.json_encode($data).')';
 }
