4846539e8bca3f1ba3d0d786dec57ea811b037f4
Jeremy Zilar <jeremyzilar@gmail.com>
2020-01-10 23:05:01 -0500

    add email to donate text

--- includes/content.php
+++ includes/content.php
@@ -2,7 +2,7 @@
 
   <p>For 19 years, <em>The Brooklyn Rail</em> has never wavered in its commitment to providing an accessible and independent forum for the arts. 2019 has been a particularly monumental year for the <em>Rail</em>, with curatorial projects at both the <a href="https://venice.brooklynrail.org">Venice Biennale</a> and the <a href="https://www.colby.edu/museum/exhibition/occupy-colby-artists-need-to-create-on-the-same-scale-that-society-has-the-capacity-to-destroy-year-2/">Colby Museum</a>, two special issues of the River Rail, <a href="https://shop.brooklynrail.org">three new releases from our literary press Rail Editions</a>, and the continued publication of our <a href="https://brooklynrail.org/subscribe">ten annual issues</a>, now read each month by 200,000 individuals around the world.</p>
 
-  <p><strong>Please donate to help keep the <em>Rail</em> independent and free for all.</strong></p>
+  <p>Please send an email to <a href="mailto:hq@brooklynrail.org">hq@brooklynrail.org</a> if you're interested in helping to <strong>keep the <em>Rail</em> independent and free for all.</strong></p>
 
   <p class="newsletter"><strong><a href="https://mailchi.mp/brooklynrail/join/?donate_page">Sign up for our newsletter</a></strong> to learn about upcoming issues and projects in 2020.</p>
 
